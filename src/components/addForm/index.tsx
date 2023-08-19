1531f801309c6ee9bc8d8d61e8d04bb90de49622
Vladimir <shues@bk.ru>
2023-08-19 04:02:32 +0300

    new

--- src/components/addForm/index.tsx
+++ src/components/addForm/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 interface addFormProps {
-    newTask:(s:string)=>void
+    newTask: (s: string) => void
 }
 
-function AddForm({newTask}:addFormProps) {
+function AddForm({ newTask }: addFormProps) {
     const [taskName, setTaskName] = React.useState<string>('');
 
-    function hendleSubmit(e:any) {
+    function hendleSubmit(e: any) {
         e.preventDefault();
         newTask(taskName);
         setTaskName('');
     }
 
     return (<form onSubmit={hendleSubmit}>
-        <input type="text" value={taskName} onChange={(e)=>setTaskName(e.target.value)}/>
-        <input type="submit" value="add todo"/>
+        <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+        <input type="submit" value="add todo" />
     </form>)
 }
 
